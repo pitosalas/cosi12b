b8014d73c799a04964cd4950bc7d67cb4324b17d
Pito Salas <pitosalas@gmail.com>
2017-11-27 17:27:39 -0500

    preparing for 2018

--- cg_config.rb
+++ cg_config.rb
@@ -16,10 +16,12 @@ COURSE_ABBREV = "12b"
 
 # Sections in the right hand margin of the page
 SECTION_CONFIG = [
-  SectionDef.new("Resources", "resources", type: :section),
+  SectionDef.new("Resources", "resources", hidden: false, type: :section),
   SectionDef.new("Extras", "extras", hidden: true, type: :section),
   SectionDef.new("Topics", "topics", hidden: true, type: :section)
 ]
 # Options are:
 # bullet_style: css style for each bullet
 STYLING_CONFIG = {bullet_style: "\"font-size: 60%; width: 10px; color: grey\""}
+HELPFUL_BOX = false
+HOME_PAGE = "/content/resources/00_syllabus.md.erb"
